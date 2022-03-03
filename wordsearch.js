17a0660d86294e6224230f047e4608f1983e4587
Harshbir Singh <singh.hveer1@gmail.com>
2022-03-03 10:53:39 -0700

    Added functionality to check word vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
     }
-}
+  }
+  //Create a new array with vertical elements
+  const verticalArray = [];
+  for (let i = 0; i < letters[i].length; i++) {
+    const newArray = [];
+    for (let j = 0; j < letters.length; j++) {
+      newArray.push(letters[j][i]);
+    }
+    verticalArray.push(newArray);
+  }
 
-module.exports = wordSearch
+  // Join vertical elements
+  const verticalJoin = verticalArray.map(ls => ls.join(''));
+  for (const i of verticalJoin) {
+    // Search for the word in array elements
+    if (i.includes(word)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+
+  
+module.exports = wordSearch;
